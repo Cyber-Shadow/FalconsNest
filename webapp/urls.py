8cde8a7d60f2aacf7250a1df6b94e1e950145e8f
Cyber-Shadow <hydrahead3355@outlook.com>
2016-06-08 21:00:52 +1000

    Favicon

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.views.generic.base import RedirectView
 from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name="index")]
+    url(r'^$', views.index, name="index"),
+]
