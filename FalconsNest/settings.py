8ea3848ced62a7ecacc18ded2feba771cce8f301
Sir-Lagsalot <gabephua@gmail.com>
2016-07-21 22:13:26 +1000

    Something wrong
    I turned debug off and it gave me 404 every time. ugh.

--- FalconsNest/settings.py
+++ FalconsNest/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
