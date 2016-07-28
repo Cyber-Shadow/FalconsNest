4c26297b6704b8154d6ababbd9c36671f1fc2673
Cyber-Shadow <hydrahead3355@gmail.com>
2016-07-28 16:40:09 +1000

    Redirect all unauthenticated users

--- webapp/views.py
+++ webapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib import auth
 from django.template.context_processors import csrf
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate
 
 def index(request):
     return render(request, "webapp/home.html")
@@ -16,11 +16,28 @@ def login(request):
     return render_to_response('webapp/login.html', c)
 
 def yournest(request):
+    try:
+        global user
+    except:
+        return HttpResponseRedirect('/register')
+    try:
+        if user is not None:
+            # the password verified for the user
+            if user == {}:
+                print(request.user.username + " was authenticated.")
+            else:
+                return HttpResponseRedirect('/register')
+        else:
+        # the authentication system was unable to verify the username and password
+            print("The username and password were incorrect.")
+    except:
+        return HttpResponseRedirect('/register')
     return render(request, 'webapp/yournest.html',
                   {'full_name': request.user.username})
 
+
 def auth_view(request):
-    global invalid
+    global invalid, user
     invalid = "False"
     username = request.POST.get('username', '')
     password = request.POST.get('password', '')
@@ -34,6 +51,7 @@ def auth_view(request):
     else:
         invalid = "True"
         return HttpResponseRedirect('/invalid')
+
 
 def invalid_login(request):
     c = {}
