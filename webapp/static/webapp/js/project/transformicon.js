9b0f28909bc424004b4a0105a4db090a527c395b
Lararium <gabrielphua@gmail.com>
2016-08-02 16:19:32 +1000

    Commit
    Dunno if I overrid anything. Sorry.

--- webapp/static/webapp/js/project/transformicon.js
+++ webapp/static/webapp/js/project/transformicon.js
@@ -1,4 +1,19 @@
-$("a").click(function(){
-  $(this).toggleClass("open");
-  $("h1").addClass("fade");
+$(function(){
+ var obj = document.querySelectorAll('.nav-icon');
+  for(var i = obj.length -1;i>=0;i--){
+      var toggle = obj[i];
+      toggleactive(toggle);
+  }
+
+  function toggleactive(toggle) {
+    toggle.addEventListener("click",function() {
+
+      if(this.classList.contains("active") === true) {
+        this.classList.remove("active");
+      }
+      else {
+        this.classList.add("active");
+      }
+    });
+  }
 });
