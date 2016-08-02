d5a5e223e93f50b496bb5d4e8e688fcb84ebf59c
Derp McDerpson <DerpfacePython@users.noreply.github.com>
2016-08-02 22:20:08 +1000

    I'm so stupid...

--- webapp/static/webapp/js/project/headerjs.js
+++ webapp/static/webapp/js/project/headerjs.js
@@ -3,12 +3,12 @@ x = 0;
 function decision() {
     x += 1;
     if (x == 1) {
-        $("mySidenav").css("width") = "250px";
-        $("main").css("marginLeft") = "250px";
+        $("mySidenav").css("width", "250px");
+        $("main").css("marginLeft", "250px");
         $("headername").toggle();
     } else {
-        $("mySidenav").css("width") = "0";
-        $("main").css("marginLeft") = "0";
+        $("mySidenav").css("width", "0");
+        $("main").css("marginLeft", "0");
         $("headername").toggle();
         x -= 2;
     }
