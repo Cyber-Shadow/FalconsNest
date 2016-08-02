2e5a0a2636d86bae675d617d113ab5688b7ad92c
Lararium <gabrielphua@gmail.com>
2016-08-03 09:54:22 +1000

    Yisss

--- webapp/static/webapp/js/project/headerjs.js
+++ webapp/static/webapp/js/project/headerjs.js
@@ -1,15 +1,20 @@
-x = 0;
+var logincheck = 0;
 
-function decision() {
-    x += 1;
-    if (x == 1) {
-        $("mySidenav").css("width", "250px");
-        $("main").css("marginLeft", "250px");
-        $("headername").toggle();
-    } else {
-        $("mySidenav").css("width", "0");
-        $("main").css("marginLeft", "0");
-        $("headername").toggle();
-        x -= 2;
-    }
+function loginUpdate() {
+  "use strict";
+  $("#loginpending").toggle();
+  $("#loggedin").toggle();
+}
+
+function launch() {
+  "use strict";
+  var loginpending = document.getElementById("loginpending").style.display;
+  var loggedin = document.getElementById("loggedin").style.display;
+  if (loginpending === "none") {
+    logincheck = 0;
+    loginUpdate();
+  } else if (loggedin === "none") {
+    logincheck = 1;
+    loginUpdate();
+  }
 }
