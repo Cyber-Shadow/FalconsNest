b81b08d0088672a9bd06284172fee99147f9e30e
Derp McDerpson <DerpfacePython@users.noreply.github.com>
2016-08-03 09:36:58 +1000

    Update headerjs.js

--- webapp/static/webapp/js/project/headerjs.js
+++ webapp/static/webapp/js/project/headerjs.js
@@ -4,11 +4,11 @@ function decision() {
     x += 1;
     if (x == 1) {
         $("mySidenav").css("width", "250px");
-        $("main").css("marginLeft", "250px");
+        $("main").css("margin-left", "250px");
         $("headername").toggle();
     } else {
         $("mySidenav").css("width", "0");
-        $("main").css("marginLeft", "0");
+        $("main").css("margin-left", "0");
         $("headername").toggle();
         x -= 2;
     }
